9aa896704e9f984e5c8f3df203f6caca7bb009e0
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-04-27 10:48:49 -0300

    adicionando consign ao index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+const db = require('./config/db')
+const consign = require('consign')
 
-//cosign
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
+
+app.db = db
 
 app.get('/', (req, res) => {
     res.status(200).send('Meu Backend')
