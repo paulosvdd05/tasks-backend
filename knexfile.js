e58ee95a601a192bd2901063d852db7778e1e656
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-04-27 09:26:43 -0300

    modificando knex.js

--- knexfile.js
+++ knexfile.js
@@ -1,39 +1,14 @@
-// Update with your config settings.
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'tasks',
+      user:     'postgres',
+      password: '728382'
     },
     pool: {
       min: 2,
