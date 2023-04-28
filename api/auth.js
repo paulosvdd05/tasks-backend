9c629c001840fdba7933e417af25664e1715ac93
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-04-28 09:12:59 -0300

    arrumando auth.js

--- api/auth.js
+++ api/auth.js
@@ -5,30 +5,35 @@ const bcrypt = require('bcrypt-nodejs')
 module.exports = app => {
     const signin = async (req, res) => {
         if (!req.body.email || !req.body.password) {
-            return res.status(400).send('Dados Incompletos')
+            return res.status(400).send('Dados incompletos')
         }
 
         const user = await app.db('users')
-            .where({ email: req.body.email })
+            .whereRaw("LOWER(email) = LOWER(?)", req.body.email)
             .first()
 
         if (user) {
             bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
-                if (err || isMatch) {
-                    return res.status(401).send()
+                if (err || !isMatch) {
+                    return res.status(401).send('Senha informada está incorreta.')
                 }
 
-                const payload = { id: user.id }
+                const payload = {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email
+                }
+
                 res.json({
                     name: user.name,
                     email: user.email,
                     token: jwt.encode(payload, authSecret),
                 })
             })
-        }else{
-            res.status(400).send('Usuário não cadastrado.')
+        } else {
+            res.status(400).send('Usuário não cadastrado!')
         }
     }
 
-    return{signin}
+    return { signin }
 }
