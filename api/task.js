64ea17cefbc19d0664560a9606cb1cada8de377b
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-05-02 08:43:20 -0300

    finalizando api

--- api/task.js
+++ api/task.js
@@ -12,55 +12,58 @@ module.exports = app => {
             .then(tasks => res.json(tasks))
             .catch(err => res.status(400).json(err))
     }
-
+    
     const save = (req, res) => {
         if (!req.body.desc.trim()) {
-            return res.status(400).send('Descrição é um campo obrigatorio')
+            return res.status(400).send('Descrição é um campo obrigatório')
         }
+
         req.body.userId = req.user.id
 
         app.db('tasks')
             .insert(req.body)
-            .then(_ =>res.status(204).send())
-            .catch(err => res.status(400).json(err))
-    }
-
-    const remove = (req, res) =>{
-        app.db('tasks')
-        .where({id: req.params.id, userId: req.user.id})
-        .del()
-        .then(rowsDeleted =>{
-            if (rowsDeleted > 0) {
-                res.status(204).send()
-            }else{
-                const msg = `Não foi encontrada task com id ${req.params.id}`
-                res.status(400).send(msg)
-            }
-        })
-        .catch(err => res.status(400).json(err))
-    }
-
-    const updateTaskDoneAt = (req, res, doneAt) =>{
-        app.db('tasks')
-            .where({id: req.params.id, userId: req.user.id})
-            .update({doneAt})
             .then(_ => res.status(204).send())
             .catch(err => res.status(400).json(err))
-
     }
-    const toggleTask = (req, res) =>{
+
+    const remove = (req, res) => {
         app.db('tasks')
-            .where({id: req.params.id, userId: req.user.id})
+            .where({ id: req.params.id, userId: req.user.id })
+            .del()
+            .then(rowsDeleted => {
+                if (rowsDeleted > 0) {
+                    res.status(204).send()
+                } else {
+                    const msg = `Não foi encontrada task com id ${req.params.id}.`
+                    res.status(400).send(msg)
+                }
+            })
+            .catch(err => res.status(400).json(err))
+    }
+
+    const updateTaskDoneAt = (req, res, doneAt) => {
+        app.db('tasks')
+            .where({ id: req.params.id, userId: req.user.id })
+            .update({ doneAt })
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(400).json(err))
+    }
+
+    const toggleTask = (req, res) => {
+        app.db('tasks')
+            .where({ id: req.params.id, userId: req.user.id })
             .first()
-            .then(task=>{
-                if(!task){
-                    const msg = `Task com id ${req.params.id}`
+            .then(task => {
+                if (!task) {
+                    const msg = `Task com id ${req.params.id} não encontrada.`
                     return res.status(400).send(msg)
                 }
+
                 const doneAt = task.doneAt ? null : new Date()
                 updateTaskDoneAt(req, res, doneAt)
             })
             .catch(err => res.status(400).json(err))
     }
-    return { getTasks, save, remove, toggleTask}
+
+    return { getTasks, save, remove, toggleTask }
 }
