b6931289ceecc789f62de7bbd2ceb4e6bea71c09
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-10-13 11:41:26 -0300

    adcio9nando porta

--- knexfile.js
+++ knexfile.js
@@ -4,18 +4,19 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-    client: 'postgresql',
-    connection: {
-      host:'postgres://paulo:X9dJXmQCRAyRtPrSaXMUbRJTkE1sSunA@dpg-ckjauo8mccbs738houh0-a/tasks_8s5d',
-      database: 'tasks_8s5d',
-      user:     'paulo',
-      password: 'X9dJXmQCRAyRtPrSaXMUbRJTkE1sSunA'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  client: 'postgresql',
+  connection: {
+    host: 'postgres://paulo:X9dJXmQCRAyRtPrSaXMUbRJTkE1sSunA@dpg-ckjauo8mccbs738houh0-a/tasks_8s5d',
+    database: 'tasks_8s5d',
+    port: 5432,
+    user: 'paulo',
+    password: 'X9dJXmQCRAyRtPrSaXMUbRJTkE1sSunA'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
 };
